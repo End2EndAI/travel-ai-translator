705f8ef01fadca2803a78a8d824cf70889ffae26
Louis Fontaine <louis.fontaine@akeneo.com>
2025-03-15 10:01:33 +0200

    increase recording duration to 60s

--- static/js/app.js
+++ static/js/app.js
@@ -60,7 +60,7 @@ async function stopRecordingAndSave() {
         audioChunks.length = 0;
 
         // Check if duration is more than 30 seconds. If so, don't transcribe.
-        if(duration > 15) {
+        if(duration > 60) {
             transcript.textContent = "Your audio recording is too long (> 15 seconds), please try again."
         } else {
             // Send the audio data to the server for transcription
